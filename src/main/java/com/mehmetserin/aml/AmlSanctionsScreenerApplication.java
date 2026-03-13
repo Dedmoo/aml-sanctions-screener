69a1b7c00145fa70f07914f3b9bb13e4099f590b
Mehmet Serin <Dedmoo@users.noreply.github.com>
2026-03-13 19:40:00 +0300

    feat: watchlist screening with Levenshtein similarity

---
+++ src/main/java/com/mehmetserin/aml/AmlSanctionsScreenerApplication.java
@@ -0,0 +1,11 @@
+package com.mehmetserin.aml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmlSanctionsScreenerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AmlSanctionsScreenerApplication.class, args);
+    }
+}
